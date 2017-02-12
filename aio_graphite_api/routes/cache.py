4945acf800bca29ce1cb238e94deae0a7f2ccfc6
Yun Xu <yunx@zillowgroup.com>
2017-02-12 15:08:38 -0800

    cache route

--- aio_graphite_api/routes/cache.py
+++ aio_graphite_api/routes/cache.py
@@ -1,1 +1,13 @@
 # carbon-cache api
+
+import aiohttp_transmute
+
+
+@aiohttp_transmute.describe(methods="GET",
+                paths="/cache/")
+async def query_carbon_cache(request, metric):
+	
+
+
+def add_carbon_cache_api(app):
+	app.router.add_transmute_route(query_carbon_cache)
