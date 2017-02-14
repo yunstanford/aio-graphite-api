cf4e4fb8abc020919ec435762f98f591b9370edd
Yun Xu <yunx@zillowgroup.com>
2017-02-13 21:36:35 -0800

    add cache api

--- aio_graphite_api/routes/cache.py
+++ aio_graphite_api/routes/cache.py
@@ -6,7 +6,13 @@ import aiohttp_transmute
 @aiohttp_transmute.describe(methods="GET",
                 paths="/cache/")
 async def query_carbon_cache(request, metric):
-	
+	if not metric:
+		return {}
+	datapoints = await request.app["conn"].query(metric)
+	return {
+		"target": metric,
+		"datapoints": datapoints
+	}
 
 
 def add_carbon_cache_api(app):
