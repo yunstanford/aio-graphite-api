692228ce0a4ad72e23a636039db1fc437bb47379
Yun Xu <yunx@zillowgroup.com>
2017-05-24 08:02:28 -0700

    restart

--- aio_graphite_api/routes/render.py
+++ aio_graphite_api/routes/render.py
@@ -1,2 +1,15 @@
 # render api
 
+import aiohttp_transmute
+
+
+@aiohttp_transmute.describe(methods="GET",
+                paths="/render/")
+async def render(request, target=None,
+				 from=None, until=None):
+	
+	
+
+
+def add_carbon_render_api(app):
+	app.router.add_transmute_route(render)
